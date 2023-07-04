a571eb0f51d728af9878c48f49c5e23a1680690b
jacek.nowaczyk <jacek.nowaczyk@vsoft.pl>
2023-07-04 23:17:41 +0200

    role description added

--- EmployeeModule/EmployeeModule.Domain/Employee.cs
+++ EmployeeModule/EmployeeModule.Domain/Employee.cs
@@ -9,5 +9,9 @@ public class Employee
     public int EvaluationPoints { get; set; }
     public DateTime HireDate { get; set; }
     public DateTime? TerminationDate { get; set; }
+    
+    /// <summary>
+    /// Employee, AdvancedEmployee or Manager
+    /// </summary>
     public string Role { get; set; }
 }
